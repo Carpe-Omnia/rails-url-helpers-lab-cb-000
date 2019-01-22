00cc2fb918256d835dbeaf65da5d8d43dd162c55
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-22 19:23:32 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-  dude = Student.create(first_name: "poop", last_name: "stain")
+  dude = Student.create(first_name: "some", last_name: "dude")
